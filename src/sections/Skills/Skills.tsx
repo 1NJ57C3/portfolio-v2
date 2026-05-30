711f07e9f39cb209e1043cb1aee2ae1035b7df49
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2026-05-30 14:36:45 -0700

    feat(skills): add columns for categorical seperation of skills

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -1,18 +1,28 @@
-import { skills } from "./SkillData";
+import { skillCategories, skills } from "./SkillData";
 // import SkillCard from "./SkillCard";
 
 function Skills() {
-  const displaySkills = skills.map((skill) => 
-    <li key={skill.id}>
-      {/* <SkillCard {...skill} /> */}
-      <p>{skill.name}</p>
-    </li>
-  );
+  const displaySkills = skillCategories.map((category) => (
+    <section key={category.name} className="skills-column">
+      <h3>{category.label}</h3>
+      <ul className="list-reset">
+        {skills.map(
+          (skill) =>
+            category.name === skill.category && (
+              <li key={skill.id} className="skills-item">
+                {/* <SkillCard {...skill} /> */}
+                <p>{skill.name}</p>
+              </li>
+            ),
+        )}
+      </ul>
+    </section>
+  ));
 
   return (
     <section className="skills" id="skills">
       <h2>Toolkit</h2>
-      <ul className="list-reset skills-grid">{displaySkills}</ul>
+      <div className="skills-grid">{displaySkills}</div>
     </section>
   );
 }
