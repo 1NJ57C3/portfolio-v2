29ec139146e848b80f64bd73d9c55c5c9d0698ae
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2026-06-23 21:12:08 -0700

    refactor(ProjectCard): add section names to classes and updates for data changes

--- src/sections/Projects/ProjectCard.tsx
+++ src/sections/Projects/ProjectCard.tsx
@@ -3,10 +3,14 @@ import type { Project } from "./ProjectData";
 function ProjectCard({ title, description }: Project) {
   return (
     <article className="project-card">
-      <img alt="" />
-      <h3>{title}</h3>
-      <p>{description.slice(0,150)}</p>
-      <button className="button-reset project-card-cta" onClick={openProjectModal}>View Project &rarr;</button>
+      <h3 className="project-card-title">{title}</h3>
+      <p className="project-card-description">
+        {description.short.slice(0, 150)}
+        {description.short.length > 150 && "..."}
+      </p>
+      <button className="button-reset project-card-cta">
+        View Project &rarr;
+      </button>
     </article>
   );
 }
