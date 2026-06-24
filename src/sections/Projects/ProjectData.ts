365a05c1273e4009dc97357df901f14907ba503f
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2026-06-23 21:03:13 -0700

    refactor(projectdata): update dummy data

--- src/sections/Projects/ProjectData.ts
+++ src/sections/Projects/ProjectData.ts
@@ -21,29 +21,38 @@ export type Project = {
 
 export type ProjectData = Project[];
 
-export const projects = [
-  {
-    id: 0,
-    title: "dummy 1",
-    description:
+const projectData = [];
+
+for (let i = 0; i < 3; i++) {
+  projectData.push({
+    title: `dummy ${i + 1}`,
+    description: {
+      short:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      full: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    motivation:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    repoURL: "/",
+    details: {
+      rationale: [],
+      architecture: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      ],
+      implementation: [
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      ],
+      tradeoffs: [],
+    },
+    links: {
+      repo: "https://github.com/1NJ57C3/",
+    },
     active: true,
-  },
-  {
-    id: 1,
-    title: "dummy 2",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    repoURL: "/",
-    active: true,
-  },
-  {
-    id: 2,
-    title: "dummy 3",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    repoURL: "/",
-    active: false,
-  },
-];
+  });
+}
+
+export const projects: Project[] = projectData.map((project, id) => ({
+  id,
+  ...project,
+}));
